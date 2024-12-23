e02767c3de8cb263d10cbb64332b2eb2b0df7c91
Florian Dupeyron <florian.dupeyron@elsys-design.com>
2024-12-23 01:17:13 +0100

    Initial, working example of simple package archive creation :dancer:

---
+++ src/verity/exchange/__init__.py
@@ -0,0 +1,6 @@
+"""
+# Exchange modules for files processing
+
+- Florian Dupeyron (florian.dupeyron@elsys-design.com)
+- December 2024
+"""
---
+++ src/verity/exchange/model_package_v1/__init__.py
@@ -0,0 +1,7 @@
+"""
+# Exchange module for model package v1 schema
+
+- Florian Dupeyron (florian.dupeyron@elsys-design.com)
+- December 2024
+
+"""
