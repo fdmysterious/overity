d5abc30f429d35cbb5e0c98b7cb146f510b6bfd0
Florian Dupeyron <florian.dupeyron@elsys-design.com>
2025-03-15 00:24:59 +0100

    Implemented basic 'verity program infos' command :)

---
+++ src/verity/backend/program.py
@@ -0,0 +1,66 @@
+"""
+# Verity program backend features
+
+- Florian Dupeyron (florian.dupeyron@elsys-design.com)
+- March 2025
+"""
+
+import logging
+
+from pathlib import Path
+
+from verity.backend.errors import ProgramNotFound
+from verity.storage.local  import LocalStorage
+
+log = logging.getLogger("backend.program")
+
+
+########################################
+# Find program.toml file
+########################################
+
+def is_program(path: Path):
+    """Indicates if the current folder is the root folder of a program"""
+    return (path / "program.toml").is_file()
+
+
+def _iter_path(pp: Path):
+    """Iterate path from cwd to filesystem root, generator style!"""
+
+    cur_path = pp
+
+    while True:
+        yield cur_path
+
+        if cur_path.parent != cur_path:
+            cur_path = cur_path.parent
+        else:
+            break
+
+
+def find_current(start_path: Path):
+    log.debug(f"Search program root folder starting from {start_path}")
+
+    if is_program(start_path):
+        return start_path
+
+    else:
+        for subpath in _iter_path(start_path.parent):
+            log.debug(f"Check parent path: {subpath}")
+
+            if is_program(subpath):
+                return sub_path
+        else:
+            raise ProgramNotFound(start_path=start_path, recursive=True)
+
+
+def infos(path: Path):
+    """Load program information"""
+
+    path = Path(path).resolve()
+
+    log.info(f"Load program information from {path}")
+
+    st = LocalStorage(path)
+
+    return st.program_info()
--- src/verity/frontend/program/infos.py
+++ src/verity/frontend/program/infos.py
@@ -5,7 +5,19 @@
 - March 2025
 """
 
+import traceback
+import textwrap
+import logging
+
+
 from argparse import ArgumentParser, Namespace
+from pathlib  import Path
+
+from verity.backend        import program as program_backend
+from verity.backend.errors import ProgramNotFound
+
+
+log = logging.getLogger("frontend.program.infos")
 
 
 def setup_parser(parser: ArgumentParser):
@@ -13,8 +25,26 @@ def setup_parser(parser: ArgumentParser):
         "infos", help="Get informations on program in current folder"
     )
 
-    pass
-
 
 def run(args: Namespace):
-    print(args)
+    cwd = Path.cwd()
+
+    try:
+        pdir    = program_backend.find_current(start_path = cwd)
+        prginfo = program_backend.infos(pdir)
+
+        print("")
+        print(f"Program information for '{prginfo.display_name}':")
+        print(f" - Slug: {prginfo.slug}")
+        print(f" - Created: {prginfo.date_created}")
+        print(f" - Initiator: {prginfo.initiator.name} <{prginfo.initiator.email}>{ ' (' + prginfo.initiator.role + ')' if prginfo.initiator.role is not None else ''}")
+        print("")
+
+        if prginfo.description is not None:
+            print(textwrap.fill(prginfo.description))
+
+
+    except ProgramNotFound as exc:
+        log.error(str(exc))
+        log.debug(traceback.format_exc())
+
