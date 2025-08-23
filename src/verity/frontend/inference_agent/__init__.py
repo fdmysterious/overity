5728ec827592269328dd41eafcd66c43312b819c
Florian Dupeyron <florian.dupeyron@elsys-design.com>
2025-08-23 19:02:26 +0200

    Basic list command for inference agents

---
+++ src/verity/frontend/inference_agent/__init__.py
@@ -0,0 +1,31 @@
+"""
+Command subgroup for inference agents related manipulations
+===========================================================
+
+- Florian Dupeyron (florian.dupeyron@elsys-design.com)
+- August 2025
+"""
+
+from argparse import ArgumentParser, Namespace
+
+from verity.frontend.inference_agent import list_cmd
+
+CLI_SUBCOMMANDS = {list_cmd}
+
+
+def setup_parser(parser: ArgumentParser):
+    subcommand = parser.add_parser(
+        "agent", aliases=["agt"], help="Inference agent manipulation"
+    )
+    subparsers = subcommand.add_subparsers(dest="agent_subcommand")
+
+    for cmd in CLI_SUBCOMMANDS:
+        subp = cmd.setup_parser(subparsers)
+        subp.set_defaults(agent_subcommand_clbk=cmd)
+
+    return subcommand
+
+
+def run(args: Namespace):
+    if hasattr(args, "agent_subcommand_clbk"):
+        args.agent_subcommand_clbk.run(args)
---
+++ src/verity/frontend/inference_agent/list_cmd.py
@@ -0,0 +1,62 @@
+"""
+List available agents in a given programme
+==========================================
+
+- Florian Dupeyron (florian.dupeyron@elsys-design.com)
+- August 2025
+"""
+
+import logging
+import traceback
+
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+from verity.backend import program as b_program
+from verity.backend import inference_agent as b_agent
+
+from verity.errors import ProgramNotFound
+
+from verity.frontend.utils import table as f_table
+
+
+log = logging.getLogger("frontend.inference_agent.list_cmd")
+
+
+def setup_parser(parser: ArgumentParser):
+    subcommand = parser.add_parser(
+        "list", aliases=["ls"], help="List available inference agents"
+    )
+    return subcommand
+
+
+def run(args: Namespace):
+    cwd = Path.cwd()
+
+    try:
+        pdir = b_program.find_current(start_path=cwd)
+
+        agents, errors = b_agent.list_agents(pdir)
+
+        # Displaying results
+        print("")
+        print(f"Found the following agents in {pdir}:")
+        print("")
+
+        headers = ("Agent path", "Agent name")
+        rows = (
+            (agt_path.relative_to(pdir), agt_info.name) for agt_path, agt_info in agents
+        )
+
+        print(f_table.table_format(headers, rows))
+
+        if errors:
+            print("")
+            print("While processing, the following errors has been found:")
+            print("")
+            for fpath, err in errors:
+                print(f"- in {fpath.relative_to(pdir)}: {err!s}")
+
+    except ProgramNotFound as exc:
+        log.exception(exc)
+        log.debug(traceback.format_exc())
